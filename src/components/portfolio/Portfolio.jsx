500e8e79913777db1b96ff12eb04e737b951feb4
unknown <emprenderusinque@hotmail.com>
2022-10-24 10:10:00 -0500

    portfolio data changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,8 @@ import { data } from './data';
 import { FormattedMessage } from 'react-intl';
 
 export const Portfolio = () => {
+
+  console.log(data.demo)
   return (
     <section id='portfolio'>
       <h5>  
@@ -28,7 +30,9 @@ export const Portfolio = () => {
               <div className='portfolio__item-cta'>
                 <h3>{ title }</h3>
                 <a href={ github } target="_blank" className='btn'>Github</a>
-                <a href={ demo } target="_blank" className='btn btn-primary' >Live Demo</a>
+                {
+                  demo && <a href={ demo } target="_blank" className='btn btn-primary' >Live Demo</a>
+                }
               </div>
             </article>
           ))
--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -2,7 +2,7 @@ import Gif from '../../assets/gifapp.jpg';
 import Users from '../../assets/users.jpg';
 import Heroes from '../../assets/heroes.jpg';
 import Calendar from '../../assets/calendar.jpg';
-
+import Comic from '../../assets/comic.PNG'
 
 export const data = [
     {
@@ -10,27 +10,30 @@ export const data = [
         image: Users,
         title: 'UsersApp',
         github: 'https://github.com/EricRusinque/users-app',
-        // demo: 'https://mern-calendar-eric.herokuapp.com/'
     },
     {
         id: 2,
         image: Calendar,
         title: 'MERN CalendarApp',
-        github: 'https://github.com',
-        demo: 'https://mern-calendar-eric.herokuapp.com/'
+        github: 'https://github.com/EricRusinque/MERN-Calendar',
     },
     {
         id: 3,
         image: Heroes,
         title: 'HeroApp',
         github: 'https://github.com/EricRusinque/hero-app',
-        // demo: 'https://mern-calendar-eric.herokuapp.com/'
     },
     {
         id: 4,
         image: Gif,
         title: 'GiftApp',
         github: 'https://github.com/EricRusinque/React-gif-expert-app',
-        // demo: 'https://mern-calendar-eric.herokuapp.com/'
+    },
+    {
+        id: 5,
+        image: Comic,
+        title: 'GiftApp',
+        github: 'https://github.com/EricRusinque/comic-book',
+        demo: 'https://ericrusinque.github.io/comic-book/'
     }
 ]
